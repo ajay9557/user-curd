cfd77dd18724d1442e6406a8e15ccf6706733f8d
Vipul <vipulrawat008@gmail.com>
2022-01-26 04:28:17 +0530

    Adding service layer
    Desc Addinng logic for validation of id, email and phone and testing with data layer mocking, 100% coverage

--- service/users/service.go
+++ service/users/service.go
@@ -1,1 +1,91 @@
 package users
+
+import (
+	"errors"
+	"regexp"
+	"user-curd/datastore"
+	"user-curd/entities"
+)
+
+type UserService struct {
+	usrStoreHandler datastore.UserStoresHandler
+}
+
+func New(handler datastore.UserStoresHandler) *UserService {
+	return &UserService{
+		usrStoreHandler: handler,
+	}
+}
+
+// validateId utility to validate the id
+func validateId(id interface{}) bool {
+
+	idInt, ok := id.(int)
+	if !ok {
+		return false
+	}
+	if idInt < 0 {
+		return false
+	}
+	return true
+}
+
+// validateEmail utility to validate email ids
+func validateEmail(email string) bool {
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return emailRegex.MatchString(email)
+}
+
+// validatePhone utility to validate phone number
+func validatePhone(phone string) bool {
+	phoneRegex := regexp.MustCompile(`^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$`)
+	return phoneRegex.MatchString(phone)
+}
+
+func (us *UserService) GetUserByIdService(id interface{}) (*entities.User, error) {
+	if validateId(id) {
+		resp, err := us.usrStoreHandler.GetUserById(id)
+		if err != nil {
+			return &entities.User{}, err
+		}
+		return resp, nil
+	}
+	return &entities.User{}, errors.New("error invalid id type")
+}
+
+func (us *UserService) GetAllUsersService() ([]*entities.User, error) {
+	return us.usrStoreHandler.GetAllUsers()
+}
+
+func (us *UserService) CreateUserService(user entities.User) error {
+	if !validateId(user.Id) {
+		return errors.New("error invalid id")
+	}
+	if !validateEmail(user.Email) {
+		return errors.New("error invalid email")
+	}
+	if !validatePhone(user.Phone) {
+		return errors.New("error invalid phone")
+	}
+	return us.usrStoreHandler.CreateUser(user)
+}
+
+func (us *UserService) UpdateUserService(user entities.User) error {
+	if !validateId(user.Id) {
+		return errors.New("error invalid id")
+	}
+	if !validateEmail(user.Email) {
+		return errors.New("error invalid email")
+	}
+	if !validatePhone(user.Phone) {
+		return errors.New("error invalid phone")
+	}
+	return us.usrStoreHandler.UpdateUser(user)
+}
+
+func (us *UserService) DeleteUserService(id interface{}) error {
+	if !validateId(id) {
+		return errors.New("error invalid id")
+	}
+	return us.usrStoreHandler.DeleteUser(id)
+}
--- service/users/service_test.go
+++ service/users/service_test.go
@@ -1,1 +1,370 @@
 package users
+
+import (
+	"errors"
+	"fmt"
+	"github.com/golang/mock/gomock"
+	"reflect"
+	"strconv"
+	"testing"
+	"user-curd/datastore"
+	"user-curd/entities"
+)
+
+func TestGetUserByIdService(t *testing.T) {
+
+	// create a controller for datastore interface
+	ctrl := gomock.NewController(t)
+	mockUserStore := datastore.NewMockUserStoresHandler(ctrl)
+	usrServ := New(mockUserStore)
+
+	testCases := []struct {
+		caseId      int
+		input       interface{}
+		mockCall    *gomock.Call
+		expectedErr error
+		expectedOut entities.User
+	}{
+		// Success case
+		{
+			caseId:      1,
+			input:       1,
+			expectedErr: nil,
+			expectedOut: entities.User{
+				Id:    1,
+				Name:  "john",
+				Email: "john91@example.com",
+				Phone: "8095765432",
+				Age:   21,
+			},
+			mockCall: mockUserStore.
+				EXPECT().
+				GetUserById(1).
+				Return(&entities.User{
+					Id:    1,
+					Name:  "john",
+					Email: "john91@example.com",
+					Phone: "8095765432",
+					Age:   21,
+				}, nil),
+		},
+		// Error validating id
+		{
+			caseId:      2,
+			expectedErr: errors.New("error invalid id type"),
+		},
+		// Error from datastore
+		{
+			caseId:      3,
+			input:       10,
+			expectedErr: errors.New("error fetching data from database"),
+			mockCall: mockUserStore.
+				EXPECT().
+				GetUserById(10).
+				Return(&entities.User{}, errors.New("error fetching data from database")),
+			expectedOut: entities.User{},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("testing "+strconv.Itoa(tc.caseId), func(t *testing.T) {
+			//mockUserStore.EXPECT().GetUserById(tc.input).Return(&tc.expectedOut, nil)
+			resp, err := usrServ.GetUserByIdService(tc.input)
+			if !reflect.DeepEqual(resp, &tc.expectedOut) {
+				t.Errorf("TestCase[%v] Expected: \t%v\nGot: \t%v\n", tc.caseId, tc.expectedOut, resp)
+			}
+			if !reflect.DeepEqual(err, tc.expectedErr) {
+				t.Errorf("TestCase[%v] Expected: \t%v\nGot: \t%v\n", tc.caseId, tc.expectedErr, err)
+			}
+		})
+	}
+
+}
+
+func TestGetAllUsersService(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	mockUserStore := datastore.NewMockUserStoresHandler(ctrl)
+	usrService := New(mockUserStore)
+
+	testCases := []struct {
+		caseId      int
+		expectedErr error
+		expectedOut []*entities.User
+	}{
+		// Success case
+		{
+			caseId:      1,
+			expectedErr: nil,
+			expectedOut: []*entities.User{
+				{
+					Id:    1,
+					Name:  "john",
+					Email: "john19@example.com",
+					Phone: "9823218772",
+					Age:   21,
+				},
+				{
+					Id:    2,
+					Name:  "jess",
+					Email: "jessH99@example.com",
+					Phone: "9821328772",
+					Age:   19,
+				},
+			},
+		},
+		// Error case
+		{
+			caseId:      2,
+			expectedErr: errors.New("error fetching data from databases"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("testing "+strconv.Itoa(tc.caseId), func(t *testing.T) {
+			mockUserStore.EXPECT().GetAllUsers().Return(tc.expectedOut, tc.expectedErr)
+			resp, err := usrService.GetAllUsersService()
+			if tc.expectedOut != nil && !reflect.DeepEqual(resp, tc.expectedOut) {
+				t.Errorf("TestCase[%v] Expected: \t%v\nGot: \t%v\n", tc.caseId, tc.expectedOut, resp)
+			}
+			if !reflect.DeepEqual(err, tc.expectedErr) {
+				t.Errorf("TestCase[%v] Expected: \t%v\nGot: \t%v\n", tc.caseId, tc.expectedErr, err)
+			}
+		})
+	}
+}
+
+func TestCreateUserService(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	mockUserStore := datastore.NewMockUserStoresHandler(ctrl)
+	usrService := New(mockUserStore)
+	fmt.Println(usrService)
+
+	testCases := []struct {
+		caseId      int
+		input       entities.User
+		mockCall    *gomock.Call
+		expectedErr error
+	}{
+		// Success case
+		{
+			caseId: 1,
+			input: entities.User{
+				Id:    3,
+				Name:  "jenny",
+				Email: "jenny27@example.com",
+				Phone: "8765234589",
+				Age:   40,
+			},
+			mockCall: mockUserStore.
+				EXPECT().
+				CreateUser(entities.User{
+					Id:    3,
+					Name:  "jenny",
+					Email: "jenny27@example.com",
+					Phone: "8765234589",
+					Age:   40,
+				}).
+				Return(nil),
+			expectedErr: nil,
+		},
+		// Case invalid id
+		{
+			caseId: 2,
+			input: entities.User{
+				Id:    -1,
+				Name:  "josh",
+				Email: "",
+				Phone: "",
+				Age:   21,
+			},
+			mockCall:    nil,
+			expectedErr: errors.New("error invalid id"),
+		},
+		// Case invalid emails
+		{
+			caseId: 3,
+			input: entities.User{
+				Id:    4,
+				Name:  "josh",
+				Email: "bad-email",
+				Phone: "1289312798",
+				Age:   21,
+			},
+			mockCall:    nil,
+			expectedErr: errors.New("error invalid email"),
+		},
+		{
+			caseId: 4,
+			input: entities.User{
+				Id:    4,
+				Name:  "josh",
+				Email: "josh-example.com",
+				Phone: "1289312798",
+				Age:   21,
+			},
+			mockCall:    nil,
+			expectedErr: errors.New("error invalid email"),
+		},
+		{
+			caseId: 5,
+			input: entities.User{
+				Id:    4,
+				Name:  "josh",
+				Email: "joshH56D$@example.com",
+				Phone: "1289312798",
+				Age:   21,
+			},
+			mockCall:    nil,
+			expectedErr: errors.New("error invalid email"),
+		},
+		// Case invalid phone
+		{
+			caseId: 6,
+			input: entities.User{
+				Id:    4,
+				Name:  "josh",
+				Email: "josh65@gmail.com",
+				Phone: "",
+				Age:   21,
+			},
+			expectedErr: errors.New("error invalid phone"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("testing "+strconv.Itoa(tc.caseId), func(t *testing.T) {
+			err := usrService.CreateUserService(tc.input)
+			if !reflect.DeepEqual(err, tc.expectedErr) {
+				t.Errorf("TestCase[%v] Expected: \t%v\nGot: \t%v\n", tc.caseId, tc.expectedErr, err)
+			}
+		})
+	}
+}
+
+func TestUpdateUserService(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	mockUserStore := datastore.NewMockUserStoresHandler(ctrl)
+	userService := New(mockUserStore)
+
+	testCase := []struct {
+		caseId      int
+		input       entities.User
+		mockCall    *gomock.Call
+		expectedErr error
+	}{
+		// Success case
+		{
+			caseId: 1,
+			input: entities.User{
+				Id:    3,
+				Name:  "josh",
+				Email: "josh22@example.com",
+				Phone: "7892678212",
+				Age:   42,
+			},
+			mockCall: mockUserStore.
+				EXPECT().
+				UpdateUser(entities.User{
+					Id:    3,
+					Name:  "josh",
+					Email: "josh22@example.com",
+					Phone: "7892678212",
+					Age:   42,
+				}).
+				Return(nil),
+			expectedErr: nil,
+		},
+		// Error cases
+		// Case invalid id
+		{
+			caseId: 2,
+			input: entities.User{
+				Id:    -1,
+				Name:  "josh",
+				Email: "",
+				Phone: "",
+				Age:   21,
+			},
+			mockCall:    nil,
+			expectedErr: errors.New("error invalid id"),
+		},
+		// Case invalid email
+		{
+			caseId: 3,
+			input: entities.User{
+				Id:    4,
+				Name:  "josh",
+				Email: "sdai@sss",
+				Phone: "1289312798",
+				Age:   21,
+			},
+			mockCall:    nil,
+			expectedErr: errors.New("error invalid email"),
+		},
+		// Case invalid phone
+		{
+			caseId: 4,
+			input: entities.User{
+				Id:    4,
+				Name:  "josh",
+				Email: "josh29@example.com",
+				Phone: "789798s999",
+				Age:   21,
+			},
+			mockCall:    nil,
+			expectedErr: errors.New("error invalid phone"),
+		},
+	}
+
+	for _, tc := range testCase {
+		t.Run("testing "+strconv.Itoa(tc.caseId), func(t *testing.T) {
+			err := userService.UpdateUserService(tc.input)
+			if !reflect.DeepEqual(err, tc.expectedErr) {
+				t.Errorf("TestCase[%v] Expected: \t%v\nGot: \t%v\n", tc.caseId, tc.expectedErr, err)
+			}
+		})
+	}
+}
+
+func TestDeleteUserService(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	mockUserStore := datastore.NewMockUserStoresHandler(ctrl)
+	userService := New(mockUserStore)
+
+	testCase := []struct {
+		caseId      int
+		input       interface{}
+		mockCall    *gomock.Call
+		expectedErr error
+	}{
+		// Success case
+		{
+			caseId: 1,
+			input:  3,
+			mockCall: mockUserStore.
+				EXPECT().
+				DeleteUser(3).Return(nil),
+			expectedErr: nil,
+		},
+		// Error case invalid id
+		{
+			caseId:      2,
+			input:       'x',
+			mockCall:    nil,
+			expectedErr: errors.New("error invalid id"),
+		},
+	}
+
+	for _, tc := range testCase {
+		t.Run("testing "+strconv.Itoa(tc.caseId), func(t *testing.T) {
+			err := userService.DeleteUserService(tc.input)
+			if !reflect.DeepEqual(err, tc.expectedErr) {
+				t.Errorf("TestCase[%v] Expected: \t%v\nGot: \t%v\n", tc.caseId, tc.expectedErr, err)
+			}
+		})
+	}
+}
