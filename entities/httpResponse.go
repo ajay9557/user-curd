d91984446292086d057d96d31fb2b7a9665aa984
Vipul <vipulrawat008@gmail.com>
2022-01-28 17:31:55 +0530

    Adding http response structure

--- entities/httpResponse.go
+++ entities/httpResponse.go
@@ -4,3 +4,9 @@ type HttpErrs struct {
 	ErrCode int    `json:"err-code"`
 	ErrMsg  string `json:"error-msg"`
 }
+
+type HttpResponse struct {
+	Data       interface{} `json:"data"`
+	Message    string      `json:"message"`
+	StatusCode int         `json:"statusCode"`
+}
