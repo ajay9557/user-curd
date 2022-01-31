bd38b75df73a80f0dad75c06de4756f093fa7bc7
dhiran7 <dhiran.kumaryadav@zopsmart.com>
2022-01-31 15:39:37 +0530

    testcases passes

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/usercurd
+
+go 1.13
+
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/golang/mock v1.6.0
+	github.com/gorilla/mux v1.8.0
+	github.com/pelletier/go-toml v1.9.4
+)
---
+++ services/users/services.go
@@ -0,0 +1,50 @@
+package users
+
+import (
+	"errors"
+	"github.com/usercurd/models"
+	"github.com/usercurd/services"
+	"github.com/usercurd/stores"
+)
+
+type userServ struct {
+	u stores.User
+}
+
+func New(st stores.User) services.User {
+	return &userServ{st}
+}
+func (u userServ) Insert(usr models.User) (models.User, error) {
+	usr, err := u.u.Insert(usr)
+	return usr, err
+}
+
+func (u userServ) GetAll() ([]models.User, error) {
+	var users []models.User
+	users, err := u.u.GetAll()
+	return users, err
+}
+
+func (u userServ) GetById(id int) (models.User, error) {
+	usr, err := u.u.GetById(id)
+	return usr, err
+}
+
+func (u userServ) Update(id int, name string) error {
+	err := u.u.Update(id, name)
+	return err
+}
+
+func (u userServ) Delete(id int) (models.User, error) {
+	// check id exists
+	usr, err := u.u.GetById(id)
+
+	if err != nil {
+		return models.User{}, errors.New("id does not exists")
+	}
+	err = u.u.Delete(id)
+	if err != nil {
+		return models.User{}, errors.New("unable to delete user")
+	}
+	return usr, err
+}
